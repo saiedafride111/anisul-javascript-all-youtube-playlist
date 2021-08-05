5c6fea7b3b5e34bbc20a7c7ac89084b68604a553
Dark coders <45978078+saiedafride111@users.noreply.github.com>
2021-08-05 05:50:54 -0800

    Update switch-case.js

--- switch-case.js
+++ switch-case.js
@@ -73,3 +73,59 @@ switch(letter) {
     default:
         console.log("Not a Vowel")
 }
+
+
+
+
+// var color = 'red';
+// if(color === 'black'){
+//     console.log("Its color is black");
+// }
+// else if(color === 'red'){
+//     console.log("Its color is red");
+// }
+// else{
+//     console.log("No color")
+// }
+
+
+// var color = 'black';
+// switch(color){
+//     case 'black':
+//         console.log("Its color is black");
+//     break;
+//     case 'white':
+//         console.log("Its color is white");
+//     break;
+//     default:
+//         console.log("No color")
+// };
+
+
+// var number = 0;
+// while(number < 10){
+//     console.log(number);
+//     if(number == 5){
+//         break;
+//     }
+//     number++;
+// }
+
+
+// var numbers = [12, 30, 60, 80, 101, 105, 150];
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     if(number > 80){
+//         break;
+//     }
+//     console.log(number);
+// }
+
+// var numbers = [12, 30, 60, 80, 101, 105, 150];
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     if(number > 80){
+//         continue;
+//     }
+//     console.log(number);
+// }
